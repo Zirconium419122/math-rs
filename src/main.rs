6b8b5d04f8c728def40c78ff0983ca07e3e70a30
Zirconium419122 <rasmus@liaskar.net>
2024-05-22 22:47:13 +0200

    Move the Expression enum into expression.rs for better clarity

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
+mod expression;
+
 use std::collections::HashMap;
-
-#[derive(Debug)]
-enum Expression {
-    Variable(char),
-    Constant(i32),
-    Addition(Box<Expression>, Box<Expression>),
-    Subtraction(Box<Expression>, Box<Expression>),
-    Multiplication(Box<Expression>, Box<Expression>),
-    // Add more operations as needed
-}
-
-impl Expression {
-    fn evaluate(&self, variable_values: &HashMap<char, i32>) -> i32 {
-        match self {
-            Expression::Variable(c) => *variable_values.get(c).unwrap_or(&0),
-            Expression::Constant(value) => *value,
-            Expression::Addition(lhs, rhs) => lhs.evaluate(variable_values) + rhs.evaluate(variable_values),
-            Expression::Subtraction(lhs, rhs) => lhs.evaluate(variable_values) - rhs.evaluate(variable_values),
-            Expression::Multiplication(lhs, rhs) => lhs.evaluate(variable_values) * rhs.evaluate(variable_values),
-        }
-    }
-}
+use expression::Expression;
 
 // Token representing a part of the input expression
 #[derive(Debug, Clone, PartialEq)]
